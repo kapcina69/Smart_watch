671babeab9990c3cb3d40d95c4b34c62c8fea267
kapcina69 <lukasmolovicpp@gmail.com>
2025-05-21 12:41:43 +0200

    Dodat font

---
+++ include/font2.h
@@ -0,0 +1,16 @@
+#ifndef font2_H
+#define font2_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_font_t font2; 
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif 
